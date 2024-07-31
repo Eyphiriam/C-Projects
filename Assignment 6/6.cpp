2313b06701c8e562afcc7c0b800e9554be17f29c
Eyphiriam <150979401+Eyphiriam@users.noreply.github.com>
2024-07-31 02:24:09 -0400

    Update 6.cpp

--- Assignment 6/6.cpp
+++ Assignment 6/6.cpp
@@ -12,42 +12,53 @@ std::condition_variable cv;
 std::mutex counter_mtx;
 int total_produced = 0;
 int total_consumed = 0;
+int active_producers = 0;
 bool all_producers_done = false;
 
-void produce(int id) {
+void produce(int id, int num_producers) {
     for (int i = 0; i < 10; ++i) {
-        std::unique_lock<std::mutex> lock(mtx);
-        buffer.push(i);
-        std::cout << "Producer " << id << " produced " << i << "\n";
-        lock.unlock();
-        cv.notify_all();
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            buffer.push(i);
+            std::cout << "Producer " << id << " produced " << i << "\n";
+        }
+        cv.notify_one(); // Notify one waiting consumer
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        std::unique_lock<std::mutex> counter_lock(counter_mtx);
-        total_produced++;
-        counter_lock.unlock();
+        {
+            std::unique_lock<std::mutex> counter_lock(counter_mtx);
+            total_produced++;
+        }
     }
-    std::unique_lock<std::mutex> lock(mtx);
-    all_producers_done = true;
-    cv.notify_all();
+
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        active_producers--;
+        if (active_producers == 0) {
+            all_producers_done = true;
+            cv.notify_all(); // Notify all consumers if all producers are done
+        }
+    }
 }
 
 void consume(int id) {
     while (true) {
         std::unique_lock<std::mutex> lock(mtx);
         cv.wait(lock, []{ return !buffer.empty() || all_producers_done; });
-        if (buffer.empty()) {
+
+        if (buffer.empty() && all_producers_done) {
             break;
         }
+
         int widget = buffer.front();
         buffer.pop();
         std::cout << "Consumer " << id << " consumed " << widget << "\n";
+
         lock.unlock();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
         std::unique_lock<std::mutex> counter_lock(counter_mtx);
         total_consumed++;
-        counter_lock.unlock();
     }
 }
 
@@ -59,10 +70,11 @@ int main(int argc, char* argv[]) {
 
     int num_producers = std::stoi(argv[1]);
     int num_consumers = std::stoi(argv[2]);
+    active_producers = num_producers;
 
     std::vector<std::thread> producers;
     for (int i = 0; i < num_producers; ++i) {
-        producers.emplace_back(produce, i);
+        producers.emplace_back(produce, i, num_producers);
     }
 
     std::vector<std::thread> consumers;
